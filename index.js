3433cb3ca5e9cdae8d2ebbb38e23d8439354410d
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-20 11:14:13 -0400

    commited

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addNewFurniture} from './src/furniture.js'
+import { addNewFurniture, getAllFurniture} from './src/furniture.js'
 
 const PORT = 3030
 const app = express()
@@ -7,5 +7,6 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send('working'))
 app.post('/furniture', addNewFurniture)
+app.get('furniture', getAllFurniture)
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`))
